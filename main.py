09b827d4dca3d33b0ce6adcca9ba6ce985186b06
Abdulroufjon <abdulroufjonm0@gmail.com>
2026-01-31 23:54:19 +0500

    vazifa yangilandi

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
-def hissobla(soat):
-    if soat <= 8:
-        return soat * 100
-    else:
-        odatiy = 8
-        qoshimcha = soat - odatiy
-
-        hammasi = odatiy * 100_000 + qoshimcha * 200_000
-        return f"oyligingiz: {hammasi}"
-
-
-user_vaqti = int(input("Ishlagan vaqtingizni kiriting: "))
-
-print(hissobla(user_vaqti))
+def hisobla(*soatlar):
+    jami = 0
+    for soat in soatlar:
+        if soat <= 8:
+            jami += soat * 100_000
+        else:
+            odatiy = 8
+            qoshimcha = soat - 8
+            jami += odatiy * 100_000 + qoshimcha * 200_000
+    return f"Oyligingiz: {jami}"
+print(hisobla(10,1,8,9))
